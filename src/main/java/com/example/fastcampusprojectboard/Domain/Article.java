62d23cc15b5fe6348c2118871a663a9122b06877
[sj.youns1027@gmail.com] <sj.youns1027@gmail.com>
2024-02-27 15:01:15 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)

---
+++ src/main/java/com/example/fastcampusprojectboard/Domain/Article.java
@@ -0,0 +1,15 @@
+package com.example.fastcampusprojectboard.Domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String content;
+    private String hashtag;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private String modifiedAt;
+    private LocalDateTime updatedBy;
+
+}
